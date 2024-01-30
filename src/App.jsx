6272948f8ae17667d298ce7050e6b2f4be79fd78
kevinmulier <kevin.mulier@outlook.fr>
2024-01-31 00:11:34 +0100

    added config UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,64 @@
 import { useState } from 'react';
 
 function App() {
-  const [colors, setColors] = useState(Array(5).fill('#ffffff'));
+  const colors = ['#FF0000', '#0000FF', '#008000', '#000000', '#800080'];
 
-  const generateRandomColors = () => {
-    const newColors = colors.map(() => {
-      const randomColor =
-        '#' +
-        Math.floor(Math.random() * 16777215)
-          .toString(16)
-          .padStart(6, 0);
-      return randomColor;
-    });
-
-    setColors(newColors);
-  };
-
-  const addColor = () => {
-    let newColors = [...colors];
-
-    const randomColor =
-      '#' +
-      Math.floor(Math.random() * 16777215)
-        .toString(16)
-        .padStart(6, 0);
-
-    newColors.unshift(randomColor);
-    setColors(newColors);
-  };
-
-  const removeColor = () => {
-    let newColors = [...colors];
-    newColors.pop();
-    setColors(newColors);
-  };
+  const [colorDuration, setColorDuration] = useState(60);
+  const [restDuration, setRestDuration] = useState(10);
+  const [randomizedColors, setRandomizedColors] = useState(colors);
 
   return (
-    <div className="min-h-dvh flex flex-col items-center justify-center gap-8">
-      <div className="flex flex-col items-center gap-4">
-        <div>
-          <h3 className="text-center">Liste des couleurs</h3>
-          <div className="flex gap-2">
-            {colors.map((color) => (
-              <input
-                type="color"
-                className="input input-ghost p-0 w-12 h-12"
-                key={Math.floor(Math.random() * 2000)}
-                value={color}></input>
-            ))}
-          </div>
-        </div>
-        <button
-          className="btn btn-accent"
-          onClick={addColor}>
-          Ajouter une couleur
-        </button>
-        <button
-          className="btn btn-neutral"
-          onClick={generateRandomColors}>
-          Couleurs aléatoires
-        </button>
-        <button
-          className="btn btn-error"
-          onClick={removeColor}>
-          Supprimer une couleur
-        </button>
+    <div className="min-h-dvh flex flex-col items-center justify-center gap-4">
+      <h1 className="text-4xl font-bold bg-gradient-to-r from-[#e79999] via-[#5858e9] to-[#55d855] inline-block text-transparent bg-clip-text">
+        SWITCH&apos;IT
+      </h1>
+      <h3 className="text-center">Liste des couleurs</h3>
+      <div className="flex flex-wrap justify-center gap-3">
+        {colors.map((color) => {
+          const bgColor = `bg-[${color}]`;
+          return (
+            <div
+              className={
+                'p-0 w-12 h-12 border-white border border-opacity-70 rounded-xl ' +
+                bgColor
+              }
+              key={color}
+              value={color}
+            />
+          );
+        })}
       </div>
-      <button className="btn btn-primary">Démarrer Switch&apos;It</button>
+      <div className="max-w-fit flex flex-col items-center mx-auto gap-2">
+        <label
+          htmlFor="color-duration"
+          className="text-center font-semibold text-lg">
+          Durée par couleur (secondes)
+        </label>
+        <input
+          id="color-duration"
+          className="input input-bordered text-center max-w-24"
+          type="number"
+          value={colorDuration}
+          onChange={({ target }) => setColorDuration(target.value)}
+        />
+      </div>
+      <div className="max-w-fit flex flex-col items-center mx-auto gap-2">
+        <label
+          htmlFor="rest-duration"
+          className="text-center font-semibold text-lg">
+          Durée entre les couleurs (secondes)
+        </label>
+        <input
+          id="rest-duration"
+          className="input input-bordered text-center max-w-24"
+          type="number"
+          value={restDuration}
+          onChange={({ target }) => setRestDuration(target.value)}
+        />
+      </div>
+      <button className="btn btn-primary btn-lg">
+        Démarrer Switch&apos;It
+      </button>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,4 +3,11 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   plugins: [require('daisyui')],
+  safelist: [
+    'bg-[#FF0000]',
+    'bg-[#0000FF]',
+    'bg-[#008000]',
+    'bg-[#000000]',
+    'bg-[#800080]',
+  ],
 };
